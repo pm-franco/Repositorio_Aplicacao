b162b19d3f34b4bf64dccc8c493f0606626af344
pm-franco <37308306+pm-franco@users.noreply.github.com>
2023-05-24 13:48:33 +0100

    scripts db updts

--- scripts_db/create_tables.sql
+++ scripts_db/create_tables.sql
@@ -17,9 +17,29 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.user_table
     OWNER to postgres;
     
--- Table: public.artwork
+
+-- Table: public.secrets
+
+-- DROP TABLE IF EXISTS public.secrets;
+
+CREATE TABLE IF NOT EXISTS public.secrets
+(
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    value character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT secrets_pkey PRIMARY KEY (type)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.secrets
+    OWNER to postgres;
+
+
 
 CREATE SEQUENCE artwork_artwork_id_seq;
+
+-- Table: public.artwork
+
 -- DROP TABLE IF EXISTS public.artwork;
 
 CREATE TABLE IF NOT EXISTS public.artwork
@@ -45,7 +65,6 @@ CREATE TABLE IF NOT EXISTS public.artwork
         REFERENCES public.user_table (email) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-        NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -54,12 +73,15 @@ ALTER TABLE IF EXISTS public.artwork
     OWNER to postgres;
 -- Index: idx_artwork_inserted_by
 
+-- DROP INDEX IF EXISTS public.idx_artwork_inserted_by;
+
 CREATE INDEX IF NOT EXISTS idx_artwork_inserted_by
     ON public.artwork USING btree
     (inserted_by COLLATE pg_catalog."default" ASC NULLS LAST)
     TABLESPACE pg_default;
 -- Index: idx_artwork_name
 
+-- DROP INDEX IF EXISTS public.idx_artwork_name;
 
 CREATE INDEX IF NOT EXISTS idx_artwork_name
     ON public.artwork USING btree
@@ -67,25 +89,24 @@ CREATE INDEX IF NOT EXISTS idx_artwork_name
     TABLESPACE pg_default;
     
     
+CREATE SEQUENCE extra_info_extra_id_seq;
+
 -- Table: public.extra_info
 
 -- DROP TABLE IF EXISTS public.extra_info;
-
-CREATE SEQUENCE extra_info_extra_id_seq;
 
 CREATE TABLE IF NOT EXISTS public.extra_info
 (
     extra_id bigint NOT NULL DEFAULT nextval('extra_info_extra_id_seq'::regclass),
     artwork_id bigint NOT NULL,
-    pdfs bytea[],
     links character varying[] COLLATE pg_catalog."default",
     info character varying[] COLLATE pg_catalog."default",
     CONSTRAINT extra_info_pkey PRIMARY KEY (extra_id),
+    CONSTRAINT extra_info_artwork_id_key UNIQUE (artwork_id),
     CONSTRAINT extra_info_artwork_id_fkey FOREIGN KEY (artwork_id)
         REFERENCES public.artwork (artwork_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-        NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -94,10 +115,55 @@ ALTER TABLE IF EXISTS public.extra_info
     OWNER to postgres;
     
     
+
+-- Table: public.layer_names
+
+-- DROP TABLE IF EXISTS public.layer_names;
+
+CREATE TABLE IF NOT EXISTS public.layer_names
+(
+    layer_name character varying COLLATE pg_catalog."default" NOT NULL,
+    multiple_points boolean NOT NULL,
+    CONSTRAINT layer_names_pkey PRIMARY KEY (layer_name)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.layer_names
+    OWNER to postgres;
+    
+
+
+-- Table: public.layers
+
+-- DROP TABLE IF EXISTS public.layers;
+
+CREATE TABLE IF NOT EXISTS public.layers
+(
+    layer_id integer NOT NULL,
+    depth integer NOT NULL,
+    image bytea NOT NULL,
+    layer_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    artwork_id integer NOT NULL,
+    CONSTRAINT layers_pkey PRIMARY KEY (layer_id),
+    CONSTRAINT layers_artwork_id_fkey FOREIGN KEY (artwork_id)
+        REFERENCES public.artwork (artwork_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.layers
+    OWNER to postgres;
+
+
+
+
+CREATE SEQUENCE zoom_point_zoom_id_seq;  
 -- Table: public.zoom_point
 
-
-CREATE SEQUENCE zoom_point_zoom_id_seq;
 -- DROP TABLE IF EXISTS public.zoom_point;
 
 CREATE TABLE IF NOT EXISTS public.zoom_point
@@ -117,12 +183,10 @@ CREATE TABLE IF NOT EXISTS public.zoom_point
     technique character varying COLLATE pg_catalog."default",
     copyrights character varying[] COLLATE pg_catalog."default",
     materials character varying[] COLLATE pg_catalog."default",
-    CONSTRAINT zoom_point_pkey PRIMARY KEY (zoom_id),
-    CONSTRAINT zoom_point_artwork_id_fkey FOREIGN KEY (artwork_id)
-        REFERENCES public.artwork (artwork_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
+    zoom_point_id integer,
+    pixel_height double precision NOT NULL,
+    pixel_width double precision NOT NULL,
+    CONSTRAINT zoom_point_pkey PRIMARY KEY (zoom_id)
 )
 
 TABLESPACE pg_default;
@@ -131,14 +195,18 @@ ALTER TABLE IF EXISTS public.zoom_point
     OWNER to postgres;
 -- Index: idx_zoom_point_layer
 
+-- DROP INDEX IF EXISTS public.idx_zoom_point_layer;
+
 CREATE INDEX IF NOT EXISTS idx_zoom_point_layer
     ON public.zoom_point USING btree
     (layer_name COLLATE pg_catalog."default" ASC NULLS LAST)
     TABLESPACE pg_default;
     
     
--- Table: public.equipment
+
 CREATE SEQUENCE equipment_equip_id_seq;
+-- Table: public.equipment
+
 -- DROP TABLE IF EXISTS public.equipment;
 
 CREATE TABLE IF NOT EXISTS public.equipment
@@ -153,10 +221,35 @@ CREATE TABLE IF NOT EXISTS public.equipment
         REFERENCES public.zoom_point (zoom_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-        NOT VALID
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.equipment
+    OWNER to postgres;
+    
+    
+    
+CREATE SEQUENCE pdf_pdf_id_seq;
+-- Table: public.pdf
+
+-- DROP TABLE IF EXISTS public.pdf;
+
+CREATE TABLE IF NOT EXISTS public.pdf
+(
+    pdf_id bigint NOT NULL DEFAULT nextval('pdf_pdf_id_seq'::regclass),
+    artwork_id bigint NOT NULL,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    file bytea,
+    link character varying COLLATE pg_catalog."default",
+    CONSTRAINT pdf_pkey PRIMARY KEY (pdf_id),
+    CONSTRAINT pdf_artwork_id_fkey FOREIGN KEY (artwork_id)
+        REFERENCES public.artwork (artwork_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.pdf
     OWNER to postgres;
